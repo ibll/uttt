10ab1e09556072ebb8a3e04799ccf578f7665811
ibll <61601241+ibll@users.noreply.github.com>
2024-09-23 22:51:46 -0500

    switched to 2 for now

--- messages/start.js
+++ messages/start.js
@@ -2,6 +2,6 @@ import { wss } from '../index.js';
 
 export default async () => {
 	wss.clients.forEach(client => {
-		client.send(JSON.stringify({ type: "create_board", size: 3 }))
+		client.send(JSON.stringify({ type: "create_board", size: 2 }))
 	});
 }
