e493abf790c4f849eeb6f3cf9caa0b50161f8aeb
ibll <61601241+ibll@users.noreply.github.com>
2024-12-22 06:28:05 +0000

    fixing default port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = https.createServer(options, loadStatic);
 
 export const wss = createWSS(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 server.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
